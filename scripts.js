8ed8784d45e83bd26216a2c45b090545a1f31c3f
Andrecruzac85 <bomber.andre@gmail.com>
2025-06-10 20:59:53 -0300

    primeiro comit

---
+++ scripts.js
@@ -0,0 +1,16 @@
+let cron
+    let number = 0
+    let h1 = document.querySelector("h1")
+
+    function start() {
+        cron = setInterval(function () {
+            number++
+            h1.innerHTML = number
+
+        }, 1000);
+
+    }
+    function stop() {
+        clearInterval(cron)
+
+    }
